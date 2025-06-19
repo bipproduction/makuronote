a37cb6393b6f47a8587c79cc5d02346523727a3e
bip production <119274726+bipproduction@users.noreply.github.com>
2025-06-19 11:05:11 +0800

    Update import-from-url.ts

--- javascript/import-from-url.ts
+++ javascript/import-from-url.ts
@@ -6,3 +6,11 @@ const minimist = (globalThis as any).minimist;
 console.log(minimist(process.argv.slice(2)));
 
 export {};
+
+
+// v2
+
+const comot = async (url: string) => (
+  new Function("window", await (await fetch(url)).text())(globalThis),
+  globalThis as any
+);
